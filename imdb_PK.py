c6ef28df87322bcfb7e853c0b12bfd94e5ce85bf
Paulina Kowalczyk <70103814+hxvrtbxvt@users.noreply.github.com>
2022-05-08 18:39:54 +0200

    Update imdb_PK.py

--- imdb_PK.py
+++ imdb_PK.py
@@ -78,12 +78,18 @@ for url in urls:
 st.dataframe(movie_df)
 
 
+#pll = movie_df["Year"].value_counts()[:10]
+#numbers = pll.index
+#quanity = pll.values
+#import plotly.express as px
+#fig = px.pie(pll,values=quanity,names=numbers,width=600, height=600)
+#fig.show()
+#st.pyplot(fig)
+
+import matplotlib.pyplot as plt
+
 pll = movie_df["Year"].value_counts()[:10]
 numbers = pll.index
 quanity = pll.values
-import plotly.express as px
-fig = px.pie(pll,values=quanity,names=numbers,width=600, height=600)
-fig.show()
-
+fig = plt.pie(pll,values=quanity,names=numbers,width=600, height=600)
 st.pyplot(fig)
-
